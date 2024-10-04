3edca395c9bd4165dfc103dda92e91be3232c929
Mudy Mud <bastien.masselon@laposte.net>
2024-10-04 18:46:33 +0200

    first commit

---
+++ expedition_cs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Specialized;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Collections.Generic;
+
+namespace expedition_cs
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            List<string> stringCollection = new List<string>();
+            stringCollection.Add("Hey");
+            stringCollection.Add("Hello");
+            stringCollection.Add("On dit quoi ?");
+            Console.WriteLine(stringCollection[1]);
+            stringCollection.RemoveAt(0);
+            stringCollection.Remove("On dit quoi ?");
+            stringCollection[0] = "Il reste plus que moi :(";
+            Console.WriteLine(stringCollection[0]);
+        }
+    }
+}
